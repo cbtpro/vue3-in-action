1fb5b402eda49f736e001f6c3899979e601a80b1
陈碧滔 <peter.chen@useful-person.com>
2021-06-22 00:03:37 +0800

    修改.env文件，配置必须以VITE开头

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+console.log(import.meta.env.VITE_VERSION)
 if (process.env.NODE_ENV === 'development') {
   import('mock2js').then(Mockjs => {
 
